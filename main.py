dea3d4ed96134e38128b76d1a44ee4413cbb6dc4
unknown <lichengxiang123@gmail.com>
2025-02-25 14:55:48 +0800

    修改 main.py page

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+
 import streamlit as st
+st.set_page_config(
+    page_title="我的应用",  # 设置页面标题
+    layout="wide",          # 可选：宽布局
+    menu_items={
+        'Get Help': None,
+        'Report a bug': None,
+        'About': None
+    }
+)
 st.title("AI Web Scrapper88888")
 url = st.text_input("Enter a website Url: ")
 if st.button("Scrape Website"):
