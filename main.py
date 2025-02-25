6f454a057a3ed4978043f39e205946f01921483c
unknown <lichengxiang123@gmail.com>
2025-02-25 14:36:06 +0800

    修改 main.py title

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.title("AI Web Scrapper88888")
 url = st.text_input("Enter a website Url: ")
 if st.button("Scrape Website"):
-    st.write("Scraping the website")
+    st.write("yes Scraping the website")
