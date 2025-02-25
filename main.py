93e62177c9d6312b8463e1ddd46d93958445e053
unknown <lichengxiang123@gmail.com>
2025-02-25 14:31:20 +0800

    修改 main.py 的描述

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title("AI Web Scrapper")
+st.title("AI Web Scrapper88888")
 url = st.text_input("Enter a website Url: ")
 if st.button("Scrape Website"):
     st.write("Scraping the website")
